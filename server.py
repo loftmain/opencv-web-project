2614b8a953028efc3b01f9bd0e324ee0cdbba6a6
Jinjae Lee <leejinjae7@gmail.com>
2020-08-14 14:18:16 +0900

    cctv 영상 분석 add

--- server.py
+++ server.py
@@ -1,63 +1,49 @@
 import cv2
 import numpy as np
-from pyzbar.pyzbar import decode
 import io
+import random
 
 from flask import Flask, request, render_template, make_response, Response
 app = Flask(__name__)
 
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_classifier = cv2.CascadeClassifier('haarcascade_eye.xml')
-vc = cv2.VideoCapture(0)
+vc = cv2.VideoCapture('vtest.avi')
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+datas = []
+
+def detectHuman(frame) :
+    detected, _ = hog.detectMultiScale(frame)
+
+    for (x, y, w, h) in detected:
+        c = (random.randint(0, 255),
+             random.randint(0, 255), 
+             random.randint(0, 255))
+        cv2.rectangle(frame, (x, y), (x + w, y + h), c, 3)
+    return detected    
 
 @app.route('/')
 def index():
-    return  render_template("face.html")
+    return  render_template("human.html")
 
-@app.route('/upload', methods=["post"])
-def upload():
-    f = request.files['file1']   
-    filename = "./static/" + f.filename
-    f.save(filename)            
-    #img = cv2.imdecode(np.fromstring(f.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-    img = cv2.imread(filename)    
-    #img = cv2.resize(img, dsize=(0, 0), fx=0.25, fy=0.25)        
-    
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray)
-    
-    if len(faces) > 0 :   
-        for (x1, y1, w1, h1) in faces:
-            cv2.rectangle(img, (x1, y1), (x1 + w1, y1 + h1), (255, 0, 255), 2)
-
-            faceROI = gray[y1:y1 + h1, x1:x1 + w1]
-            cv2.imshow('face', faceROI)                
-
-            eyes = eye_classifier.detectMultiScale(faceROI)
-            for (x2, y2, w2, h2) in eyes:
-                center = (int(x1+ x2 + w2 / 2), int(y1 +y2 + h2 / 2))
-                cv2.circle(img, center, int(w2 / 2), (255, 0, 0), 2, cv2.LINE_AA)
-
-        retval, buffer = cv2.imencode('.jpg', img)  
-
-
-        response = make_response(buffer.tobytes())
-        response.headers['Content-Type'] = 'image/jpg'
-
-        return response    
-    
-    code = decode(img)    
-    url  = code[0].data.decode("utf-8")    
-    
-    return f"<a href={url}>QR코드가기</a>" 
-    
-    #return  f"<script> alert('이동합니다.'); window.location.href='{url}'; </script>"
-    
-    
-    
 def gen():
+    global vc
+    global datas
+    
     while True:
         read_return_code, frame = vc.read()
+        
+        if not read_return_code :
+            vc = cv2.VideoCapture('vtest.avi')
+            datas = []
+            break;
+        
+        rect = detectHuman(frame)
+        
+        datas.append(len(rect))
+        print(datas)
+        
         encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
         io_buf = io.BytesIO(image_buffer)
         yield (b'--frame\r\n'
@@ -72,11 +58,10 @@ def video_feed():
         mimetype='multipart/x-mixed-replace; boundary=frame'
     )
 
-@app.route('/test')
-def test():
-    return  "<h1>동영상 테스트</h1> <img src=/video_feed width=320 height=240>"
-
-
-
+@app.route('/view')
+def view():
+    global datas
+    return str(datas)
+    
 if __name__ == '__main__':
      app.run(host='0.0.0.0', debug=True, port=8000)      
